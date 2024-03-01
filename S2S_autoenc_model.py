8ee4d5062205726d725862d0537f3506db6b10d4
Nathan H. Arnett <arnett.nate@gmail.com>
2024-03-01 20:14:48 +0900

    Added sample size for training

--- S2S_autoenc_model.py
+++ S2S_autoenc_model.py
@@ -33,7 +33,7 @@ class Autoencoder(Model):
     def compile_model(self, optimizer='adam', loss=losses.MeanAbsoluteError()):
         self.compile(optimizer=optimizer, loss=loss)
 
-    def fit_training_sound(self, filename, epochs=100, batch_size=32, samples=100, snippet_len=128*128, mels=700):
+    def fit_training_sound(self, filename, epochs=100, batch_size=64, samples=100, snippet_len=128*128, mels=700):
         sound, sr = librosa.load(filename)
         sound_matrix_size = int((len(sound) / snippet_len) // samples)
 
@@ -45,7 +45,7 @@ class Autoencoder(Model):
         X_train = sound_trimmed.reshape(sound_matrix_size * samples, snippet_len)
 
         # Generate Mel spectrograms
-        X_train_mels = librosa.feature.melspectrogram(y=X_train, sr=sr, n_fft=2500, hop_length=500, n_mels=mels)
+        X_train_mels = librosa.feature.melspectrogram(y=X_train, sr=sr, n_fft=2400, hop_length=500, n_mels=mels)
         X_train_mels_db = librosa.power_to_db(X_train_mels)
 
         print("----Fitting Data----")
@@ -61,14 +61,14 @@ class Autoencoder(Model):
 
         print("~~~~Warping sample~~~~")
         sample_trimmed = sample[:snippet_len]
-        sample_mels = librosa.feature.melspectrogram(y=sample_trimmed, sr=sr, n_fft=2500, hop_length=500, n_mels=700)
+        sample_mels = librosa.feature.melspectrogram(y=sample_trimmed, sr=sr, n_fft=2400, hop_length=500, n_mels=700)
         sample_mels_db = librosa.power_to_db(sample_mels)
         sample_mels_db_reshaped = sample_mels_db.reshape(1, *self.model_input_shape)
 
         encoded_mel = self.encoder(sample_mels_db_reshaped)
         decoded_mel = self.decoder(encoded_mel).numpy().squeeze(0)
 
-        warped_sample = librosa.feature.inverse.mel_to_audio(decoded_mel, n_fft=2500, sr=sr)
+        warped_sample = librosa.feature.inverse.mel_to_audio(decoded_mel, n_fft=2400, sr=sr)
         return warped_sample
 
     def generate_random_sounds(self, n_samples, mean=0, scale=1):
@@ -77,7 +77,7 @@ class Autoencoder(Model):
         for n in range(n_samples):
             normal_noise = np.random.normal(loc=mean, scale=scale, size=(1, self.latent_dim))
             decoded_mel = self.decoder(normal_noise).numpy().squeeze(0)
-            random_sound = librosa.feature.inverse.mel_to_audio(decoded_mel,  n_fft=2500, sr=self.sample_rate)
+            random_sound = librosa.feature.inverse.mel_to_audio(decoded_mel,  n_fft=2400, sr=self.sample_rate)
             sample_list.append(random_sound)
 
         return sample_list
@@ -87,14 +87,14 @@ if __name__ == "__main__":
     autoencoder.compile_model()
 
     ''' Train the model with a sound '''
-    autoencoder.fit_training_sound('./audio/AMINOR.wav', epochs=50)
+    autoencoder.fit_training_sound('./audio_bank/SYNTH_track.wav', epochs=250, samples=10)
 
     ''' Create hybrid sound '''
-    warped = autoencoder.warp_sample('./audio/SYNTH_sample.wav')
-    sf.write(f"./warped_sound_{time.time()}.wav", warped, autoencoder.sample_rate)
+    #warped = autoencoder.warp_sample('./audio_bank/SYNTH_sample.wav')
+    #sf.write(f"./audio_bank/warped_sound_{time.time()}.wav", warped, autoencoder.sample_rate)
 
     ''' Generate random sounds '''
-    #warped_noise_sample = autoencoder.generate_random_sounds(n_samples=3, mean=0, scale=3)
+    #warped_noise_sample = autoencoder.generate_random_sounds(n_samples=2, mean=6, scale=1)
 
     #for x in range(len(warped_noise_sample)):
-        #sf.write(f"./warped_sound_{time.time()}.wav", warped_noise_sample[x], autoencoder.sample_rate)
+        #sf.write(f"./audio_bank/warped_sound_{time.time()}.wav", warped_noise_sample[x], autoencoder.sample_rate)
