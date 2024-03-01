e153a2f9e3c8df92ec3a529752170e94d804b5e5
Nathan Arnett <arnett.nate@gmail.com>
2024-03-01 20:13:15 +0900

    added sample length or training

--- S2S_autoenc_model.py
+++ S2S_autoenc_model.py
@@ -1,149 +1,100 @@
 import tensorflow as tf
+from tensorflow.keras import layers, losses, Model
 import librosa
 import numpy as np
-from tensorflow.keras import layers, losses
-from tensorflow.keras.models import Model
+import soundfile as sf
+import time
 
 class Autoencoder(Model):
-    def __init__(self, shape=(700, 33), latent_dim=300):
+    def __init__(self, input_shape=(700, 33), latent_dim=300):
         super(Autoencoder, self).__init__()
-
         self.latent_dim = latent_dim
-        self.SAMPLE_RATE = 22050
-        self.shape = shape
+        self.sample_rate = 22050
+        self.model_input_shape = input_shape
+
+        # Encoder architecture
         self.encoder = tf.keras.Sequential([
-
-          layers.Conv1D(2, 3, activation='relu', input_shape=shape),
-
-          # For your consideration layers to implement.
-          #layers.Conv1D(3, 6, activation='relu'),
-          #layers.Dense(1000, activation='linear'),
-          #layers.Dense(25, activation='relu'), #relu?
-
-          layers.Flatten(),
-          layers.Dense(latent_dim, activation='relu'), #relu?
+            layers.Conv1D(2, 3, activation='relu', input_shape=input_shape),
+            layers.Flatten(),
+            layers.Dense(latent_dim, activation='relu'),
         ])
 
+        # Decoder architecture
         self.decoder = tf.keras.Sequential([
-
-          #layers.Conv1DTranspose(16, 6, activation='relu'),
-          #layers.Conv1DTranspose(32, 3, activation='relu'),
-
-          layers.Dense(tf.math.reduce_prod(shape), trainable=True, activation = 'linear'),
-          layers.Reshape(shape),
+            layers.Dense(tf.math.reduce_prod(input_shape), activation='linear'),
+            layers.Reshape(input_shape),
         ])
 
-    def call(self, x):
-        '''also used when .fit() ?'''
-        encoded = self.encoder(x)
+    def call(self, inputs):
+        encoded = self.encoder(inputs)
         decoded = self.decoder(encoded)
         return decoded
 
-    def compile_me(self, optimizer='adam', loss=losses.MeanAbsoluteError()):
-
+    def compile_model(self, optimizer='adam', loss=losses.MeanAbsoluteError()):
         self.compile(optimizer=optimizer, loss=loss)
 
-
-    def fit_training_sound(self, filename,
-                            verbose = 1,
-                            epochs = 100,
-                            batch_size = 32,
-                            samples = 100,
-                            snippet_len = 128*128,
-                            mels = 700):
-        ''' mels and snippets define the SHAPE information
-            going to the autoencoder. This SHAPE information
-            can be dynamic.
-
-            More mels implies higher resolution sound.
-            hop length with affect the time.
-
-        '''
+    def fit_training_sound(self, filename, epochs=100, batch_size=64, samples=100, snippet_len=128*128, mels=700):
         sound, sr = librosa.load(filename)
+        sound_matrix_size = int((len(sound) / snippet_len) // samples)
 
-        sound_matrix_size = int(((sound.shape[0] / snippet_len) // samples))
+        if sound_matrix_size < 1:
+            print("Not enough information. Please input a larger sound file.")
+            return
 
-        if sound_matrix_size < 1: #is not_large_enough
-            print("Not enough information. Input larger sound file.")
+        sound_trimmed = sound[:sound_matrix_size * samples * snippet_len]
+        X_train = sound_trimmed.reshape(sound_matrix_size * samples, snippet_len)
 
-        else:
+        # Generate Mel spectrograms
+        X_train_mels = librosa.feature.melspectrogram(y=X_train, sr=sr, n_fft=2400, hop_length=500, n_mels=mels)
+        X_train_mels_db = librosa.power_to_db(X_train_mels)
 
-            #edit sound,
-            range = sound_matrix_size*samples*snippet_len
+        print("----Fitting Data----")
+        self.fit(X_train_mels_db, X_train_mels_db, epochs=epochs, batch_size=batch_size, shuffle=True, verbose=1)
 
-            X_train = sound[:range].reshape(sound_matrix_size*samples, snippet_len)
-
-            #generate spectograms of the sound snippets
-            X_train_mels = librosa.feature.melspectrogram(
-                    y=X_train,
-                    sr=sr, n_fft=2500,
-                    hop_length=500,
-                    n_mels=mels)
-
-            #decibel scaler, essentially log scaling.
-            X_train_mels_db = librosa.power_to_db(X_train_mels)
-
-            try:
-
-                print("""
-                      ----Fitting Data----
-                      """)
-                self.fit(X_train_mels_db,
-                         X_train_mels_db,
-                        epochs=epochs,
-                        batch_size = batch_size,
-                        shuffle=True,
-                        verbose = verbose)
-
-            except:
-                print("""something went wrong while fitting.
-                         check that the shape is correct.""")
-
-
-    def warp_sample(self, filename, snippet_len = 128*128,):
-
+    def warp_sample(self, filename, snippet_len=128*128):
         sample, sr = librosa.load(filename)
-
-        sample_size = int(sample.shape[0] / (128*128) // 1 )
+        sample_size = int(len(sample) / snippet_len // 1)
 
         if sample_size < 1:
             print("Audio sample too small.")
+            return
 
-        else:
-            print("~~~~warping sample~~~~")
-            sample_edit = sample[:snippet_len]
-            sample_mels = librosa.feature.melspectrogram(y=sample_edit,
-                                            sr=sr, n_fft=2500,
-                                            hop_length=500,
-                                            n_mels=700)
+        print("~~~~Warping sample~~~~")
+        sample_trimmed = sample[:snippet_len]
+        sample_mels = librosa.feature.melspectrogram(y=sample_trimmed, sr=sr, n_fft=2400, hop_length=500, n_mels=700)
+        sample_mels_db = librosa.power_to_db(sample_mels)
+        sample_mels_db_reshaped = sample_mels_db.reshape(1, *self.model_input_shape)
 
-            sample_mels_db = librosa.power_to_db(sample_mels)
+        encoded_mel = self.encoder(sample_mels_db_reshaped)
+        decoded_mel = self.decoder(encoded_mel).numpy().squeeze(0)
 
-            sample_mels_db_reshaped = sample_mels_db.reshape(1, 700, 33)
+        warped_sample = librosa.feature.inverse.mel_to_audio(decoded_mel, n_fft=2400, sr=sr)
+        return warped_sample
 
-            encoded_mel = self.encoder(sample_mels_db_reshaped)
-            decoded_mel = self.decoder(encoded_mel)
+    def generate_random_sounds(self, n_samples, mean=0, scale=1):
+        sample_list = []
+        print("~~~~Generating noise samples~~~~")
+        for n in range(n_samples):
+            normal_noise = np.random.normal(loc=mean, scale=scale, size=(1, self.latent_dim))
+            decoded_mel = self.decoder(normal_noise).numpy().squeeze(0)
+            random_sound = librosa.feature.inverse.mel_to_audio(decoded_mel,  n_fft=2400, sr=self.sample_rate)
+            sample_list.append(random_sound)
 
-            decoded_mel = np.squeeze(decoded_mel, axis=0)
-
-            warped_sample = librosa.feature.inverse.mel_to_audio(decoded_mel,  n_fft=2500, sr=sr)
-
-            return warped_sample
-
-
+        return sample_list
 
 if __name__ == "__main__":
+    autoencoder = Autoencoder(latent_dim=300)
+    autoencoder.compile_model()
 
-    import time
-    import soundfile as sf
-    auto_enc = Autoencoder()
-    auto_enc.compile_me()
+    ''' Train the model with a sound '''
+    autoencoder.fit_training_sound('./audio_bank/SYNTH_track.wav', epochs=250, samples=10)
 
-    auto_enc.fit_training_sound('./AMINOR.wav',epochs=10)
-    warped = auto_enc.warp_sample('./SYNTH_piano.mp3')
+    ''' Create hybrid sound '''
+    #warped = autoencoder.warp_sample('./audio_bank/SYNTH_sample.wav')
+    #sf.write(f"./audio_bank/warped_sound_{time.time()}.wav", warped, autoencoder.sample_rate)
 
-    sf.write(f"warped_sound_{time.time()}.wav", warped, auto_enc.SAMPLE_RATE)
+    ''' Generate random sounds '''
+    #warped_noise_sample = autoencoder.generate_random_sounds(n_samples=2, mean=6, scale=1)
 
-    #ipd.Audio(warped, rate=)
-    #print(auto_enc.encoder.summary())
-    #print(auto_enc.decoder.summary())
+    #for x in range(len(warped_noise_sample)):
+        #sf.write(f"./audio_bank/warped_sound_{time.time()}.wav", warped_noise_sample[x], autoencoder.sample_rate)
