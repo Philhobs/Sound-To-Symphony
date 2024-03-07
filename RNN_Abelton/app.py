6ea3e5bf3e752a95fe26956fc463adaf02cb77a6
Christelle Joseph <Christellejoseph@gmail.com>
2024-03-07 14:48:03 +0900

    added upload file

--- RNN_Abelton/app.py
+++ RNN_Abelton/app.py
@@ -3,6 +3,7 @@ from pitch_list import pitch
 from RNN_model import RNN_Model
 import tensorflow as tf
 import pretty_midi
+import os
 
 
 def generate_colored_text(text, colors):
@@ -21,7 +22,25 @@ notes = "♩♫♪"
 colored_title = generate_colored_text(title, rainbow_colors)
 st.markdown(align_text(colored_title, 'center'), unsafe_allow_html=True)
 
+#***** code *****
 
+## construct model
+rnn = RNN_Model()
+rnn.compile_model()
+checkpoint_path = './training_checkpoints/ckpt_5'
+rnn.model.load_weights(checkpoint_path)
+
+
+uploaded_file = st.file_uploader(" ",type=["mid"])
+
+if uploaded_file is not None:
+    input_file = uploaded_file.name
+    file_path = os.path.join('', input_file)
+    with open(file_path, 'wb') as f:
+        f.write(uploaded_file.read())
+    pm = pretty_midi.PrettyMIDI(file_path)
+    instrument = pm.instruments[0]
+    instrument_name = pretty_midi.program_to_instrument_name(instrument.program)
 
 option = st.selectbox(
    "Select a pitch",
@@ -34,25 +53,8 @@ st.write('You selected:', option)
 st.write("")
 
 
-## construct model
-rnn = RNN_Model()
-## compling
-rnn.compile_model()
-## load weight
-checkpoint_path = './training_checkpoints/ckpt_5'
-rnn.model.load_weights(checkpoint_path)
-## get instrument name from input
-input_path = 'test.midi' # !!!please set input file!!!
-pm = pretty_midi.PrettyMIDI(input_path)
-instrument = pm.instruments[0]
-instrument_name = pretty_midi.program_to_instrument_name(instrument.program)
-## prediction
 key_name = ''
-# gen_notes = rnn.generate_notes_from_midi_file(input_path, key = key_name)
-
 if st.button('Generate Midi'):
-    gen_notes = rnn.generate_notes_from_midi_file(input_path, key=option)
-    ## midi output
+    gen_notes = rnn.generate_notes_from_midi_file(file_path, key=option)
     output_path = 'CHRISTELLE_1.midi'
     output_midi = rnn._notes_to_midi(gen_notes, out_file = output_path, instrument_name = instrument_name)
-    # output_path is midi file. I... dont know why it return output_midi for... maybe for playing music at the notebook
