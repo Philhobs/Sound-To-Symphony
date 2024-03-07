75c08ea672c4c1802e03a4adbce11fd600dba35a
Christelle Joseph <Christellejoseph@gmail.com>
2024-03-07 13:52:36 +0900

    connected app to backend

--- RNN_Abelton/app.py
+++ RNN_Abelton/app.py
@@ -32,30 +32,27 @@ option = st.selectbox(
 st.write('You selected:', option)
 
 st.write("")
-st.button('Generate Music')
+
 
 ## construct model
 rnn = RNN_Model()
-
 ## compling
 rnn.compile_model()
-
 ## load weight
 checkpoint_path = './training_checkpoints/ckpt_5'
 rnn.model.load_weights(checkpoint_path)
-
 ## get instrument name from input
 input_path = 'test.midi' # !!!please set input file!!!
 pm = pretty_midi.PrettyMIDI(input_path)
 instrument = pm.instruments[0]
 instrument_name = pretty_midi.program_to_instrument_name(instrument.program)
-
 ## prediction
-key_name = 'C_minor'
-gen_notes = rnn.generate_notes_from_midi_file(input_path, key = key_name)
+key_name = ''
+# gen_notes = rnn.generate_notes_from_midi_file(input_path, key = key_name)
 
-## midi output
-output_path = 'new.midi'
-output_midi = rnn._notes_to_midi(gen_notes, out_file = output_path, instrument_name = instrument_name)
-# output_path is midi file. I... dont know why it return output_midi for... maybe for playing music at the notebook
-
+if st.button('Generate Midi'):
+    gen_notes = rnn.generate_notes_from_midi_file(input_path, key=option)
+    ## midi output
+    output_path = 'CHRISTELLE_1.midi'
+    output_midi = rnn._notes_to_midi(gen_notes, out_file = output_path, instrument_name = instrument_name)
+    # output_path is midi file. I... dont know why it return output_midi for... maybe for playing music at the notebook
